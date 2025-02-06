404b7b816e25a225afe40e7830ba080cc5a87ed4
Andres L <darkobito@gmail.com>
2025-02-07 12:34:35 +1300

    Modifying Files

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+// TRYING TO DRAW AMPLITUD AND FREQUENCY
+
 var song;
 var vol;
 var amp;
@@ -6,10 +8,9 @@ var img;
 var r = 235;
 var g = 64;
 
-function preload (){
-  song = loadSound ('sound/cockatoo.mp3');
-  img = loadImage ('images/frog.png');
-  background (255,0,0);
+function preload() {
+  song = loadSound('sound/cockatoo.mp3');
+  img = loadImage('images/frog.png');
 }
 
 function setup() {
@@ -17,42 +18,62 @@ function setup() {
 
   // setting definitions to retrieve data
 
-  amp = new p5.Amplitude ();
-  fft = new p5.FFT ();
+amp = new p5.Amplitude();
+fft = new p5.FFT();
 
-// Loop and playing the song 
+  // Loop and playing the song 
 
-  song.play ();
-  song.loop ();
+song.play();
+song.loop();
 }
 
 function draw() {
   background(220);
 
-  // Drawing the values I have retrieve from sound
+    // Drawing the values I have retrieve from sound
 
-  vol = amp.getLevel ();
-  var spectrum = fft.analyze ();
+  vol = amp.getLevel();
+  var spectrum = fft.analyze();
+
+  // Drawing amplitude
+  fill(255, 0, 0);
+  noStroke();
+  rect(10, 10, vol * 100, 20);
+
+  // Drawing frequency
+  // Creating an spectrum array that loop
+
+  fill(0, 0, 255);
+  noStroke();
+
+  //The map() function converts a value from one range to another.
+  
+  for (var i = 0; i < spectrum.length; i++) {
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = map(spectrum[i], 0, 255, 0, height);
+    rect(x, height - h, 2, h);
+  }
 
   //This is how I map the frequency spectrum to Colour Values
 
-  r = map (spectrum [20],0,255,64,234);
-  g = map (spectrum [50], 0, 255, 64, 234);
-  tint (r,g, 52);
-  image (img, width/2-50, (height-100)-(vol*100), 100, 50);
+  r = map(spectrum[20], 0, 255, 235, 150);
+  g = map(spectrum[50], 0, 255, 64, 234);
+  tint(r, g, 52);
+  image(img, width/2-50, (height-100)-(vol*100), 100, 50);
 }
 
 // Using the mouse to execute movement
 
-function mousePressed (){
+function mousePressed() {
 
-  // creating a conditional, isPlaying work in way that returns the boolean
+// creating a conditional, isPlaying work in way that returns the boolean
+// boolean() converts values to true or false
 
-  if (song.isPlaying()){
+  if (song.isPlaying()) {
     song.stop();
-    background (255,0,0);
+    background(255, 0, 0);
   } else {
-    song.play ();
-    background (0,255,0);
+    song.play();
+    background(0, 255, 0);
   }
 }
