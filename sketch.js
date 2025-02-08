8c6cefcc656d71be1ccc72ac259575a76706c601
Andres L <darkobito@gmail.com>
2025-02-08 16:55:20 +1300

    Adding all images
    Tweeking
    Adding images
    Index by images
    Rotating
    Text

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-// ADDING WARNING MESSAGE (WORKING)
-// Tweek
-// Creating New variables to start loop by conditionals
-// Creating New variables to start text by conditionals
+// WARNING MESSAGE (WORKING)
+// TEXT NO WORKING AS I WANT (Will modified)
+// ADDING ALL IMAGES
+// MAKING ONE OF THE IMAGES ROTATE (no fully ready)
 
 let song1, song2, song3, song4, song5;
 let vol;
 let amp;
 let fft;
-let img;
+let img, img1, img2, img3, img4, img5;
 let particles = [];
 let started = false;
 let heartRate = 1;
@@ -16,22 +16,29 @@ let submitButton;
 let currentSong;
 let showEmergencyMessage = false;
 let emergencyTimeout = null;
+let angle = 0;
 
+// Loading new images (The house is shaking_would it work)
 function preload() {
-song1 = loadSound('sound/heartsound1.mp3');
+song1 = loadSound('sound/climbing.mp3');
 song2 = loadSound('sound/heartsound2.mp3');
 song3 = loadSound('sound/heartsound3.mp3');
 song4 = loadSound('sound/heartsound4.mp3');
 song5 = loadSound('sound/heartsound5.mp3');
-img = loadImage('images/frog.png');
+img = loadImage('images/singer.png');
+img1 = loadImage('images/monster.png');
+img2 = loadImage('images/rightm.png');
+img3 = loadImage('images/leftm.png');
+img4 = loadImage('images/musicnotes.png');
+img5 = loadImage('images/floor.png');
 }
 
 function setup() {
 createCanvas(1500, 1000);
-// setting up definitions to retrieve data
+// Retrieving data
 amp = new p5.Amplitude();
 fft = new p5.FFT();
-// Array of particles
+// Particles Array
 for (var i = 0; i < 500; i++) {
 particles.push(new Particle());
 }
@@ -39,21 +46,19 @@ particles.push(new Particle());
 inputBox = createInput('');
 inputBox.position(650, 1010);
 inputBox.size(100, 30);
-
-// Submit button
+//Submit button
 submitButton = createButton('Submit');
 submitButton.position(760, 1012);
 submitButton.size(100, 30);
 
 // Submit Button
-// created statement
+// Statements
 // Use parseInt which retrieves values and convert it to integer (number)
 // Use isNaN to check if the value is not a number convert to value 1.
-// Also an Array for the particles to move randomly
+// Array for the particles to move randomly
 // Expect loop to work after submit button
-// Set velocity on the particles for the movement (vx,vy)
-// added conditionals depending input data
-
+// Velocity on the particles for the movement (vx,vy)
+// Conditionals depending input data
 submitButton.mousePressed(() => {
 heartRate = parseInt(inputBox.value());
 if (isNaN(heartRate)) {
@@ -63,26 +68,27 @@ if (started) {
 currentSong.stop();
 }
 started = true;
+angle = 0;
 if (heartRate < 60 || heartRate > 180) {
 showEmergencyMessage = true;
 if (emergencyTimeout !== null) {
 clearTimeout(emergencyTimeout);
-}
-emergencyTimeout = setTimeout(() => {
-window.location.reload();
-}, 2000);
-} else {
-showEmergencyMessage = false;
-if (emergencyTimeout !== null) {
-clearTimeout(emergencyTimeout);
-}
-if (heartRate >= 60 && heartRate < 75) {
-currentSong = song1;
-} else if (heartRate >= 75 && heartRate < 90) {
-currentSong = song2;
-} else if (heartRate >= 90 && heartRate < 105) {
-currentSong = song3;
-} else if (heartRate >= 105 && heartRate < 120) {
+ }
+ emergencyTimeout = setTimeout(() => {
+ window.location.reload();
+ }, 2000);
+ } else {
+ showEmergencyMessage = false;
+ if (emergencyTimeout !== null) {
+ clearTimeout(emergencyTimeout);
+ }
+ if (heartRate >= 60 && heartRate < 75) {
+ currentSong = song1;
+ } else if (heartRate >= 75 && heartRate < 90) {
+ currentSong = song2;
+ } else if (heartRate >= 90 && heartRate < 105) {
+ currentSong = song3;
+ } else if (heartRate >= 105 && heartRate < 120) {
 currentSong = song4;
 } else if (heartRate >= 120 && heartRate < 135) {
 currentSong = song5;
@@ -102,14 +108,40 @@ particles[i].size = random(2, 5);
 
 function draw() {
 background(0);
-// values for retrieving sound data
+// Values for retrieving sound data
 // New conditionals
 // Modified Amplitud
-if (started) {
+if (!started) {
+fill(255);
+textSize(32);
+text("Please type your hear rate value and click submit", width / 2 - 300, height / 2);
+inputBox.show();
+submitButton.show();
+} else {
+// Draw img4 with rotation and animation
+// Draw the image and offsetting it on X, to create movement effect
+// Separating functions by images movement
+// Use Vol frequency for movement
+// Adding indication text
+// Adding index to each picture depending of the action I want
+imageMode(CENTER);
+push();
+translate(width / 2, height / 2);
+rotate(radians(angle));
+var img4Size = map(amp, 0, 1, 50, 200);
+image(img4, 0, 0, img4Size, img4Size);
+pop();
+    
+// Draw other images
+image(img2, width / 2, height / 2, img1.width, img1.height);
+image(img3, width / 2, height / 2, img1.width, img1.height);
+image(img5, width / 2, height / 2, img1.width, img1.height);
+    
 if (showEmergencyMessage) {
 fill(255, 0, 0);
 textSize(32);
 text("Call Emergency", width / 2 - 150, height / 2);
+angle = 0; // Reset the angle
 } else {
 vol = amp.getLevel();
 var spectrum = fft.analyze();
@@ -122,39 +154,42 @@ var rIndex = i;
 var gIndex = i + 1;
 var bIndex = i + 2;
 var aIndex = i + 3;
-//map() function converts a value from one range to another.
 var freqIndex = floor(map(i, 0, img.pixels.length, 0, spectrum.length));
-//map the frequency spectrum to Colour Values
 img.pixels[rIndex] = map(spectrum[freqIndex % spectrum.length], 0, 255, 0, 255);
 img.pixels[gIndex] = map(spectrum[(freqIndex + 10) % spectrum.length], 0, 255, 0, 255);
 img.pixels[bIndex] = map(spectrum[(freqIndex + 20) % spectrum.length], 0, 255, 0, 255);
 }
-// Draw particles and array loop (Using spectrum)
 img.updatePixels();
+img1.loadPixels();
+for (var i = 0; i < img1.pixels.length; i += 4) {
+var rIndex = i;
+var gIndex = i + 1;
+var bIndex = i + 2;
+var aIndex = i + 3;
+var freqIndex = floor(map(i, 0, img1.pixels.length, 0, spectrum.length));
+img1.pixels[rIndex] = map(spectrum[freqIndex % spectrum.length], 0, 255, 0, 255);
+img1.pixels[gIndex] = map(spectrum[(freqIndex + 10) % spectrum.length], 0, 255, 0, 255);
+img1.pixels[bIndex] = map(spectrum[(freqIndex + 20) % spectrum.length], 0, 255, 0, 255);
+}
+img1.updatePixels();
 for (var i = 0; i < particles.length; i++) {
 particles[i].update(spectrum);
 particles[i].display(spectrum, heartRate);
 }
-}
-// Draw the image and offsetting it on X, to create movement effect
-// Use Vol frequency for movement
-// I think I am becoming crazy
-// Adding indication text
 imageMode(CENTER);
 push();
-var offsetX = map(vol, 0, 1, -100, 100);
+var offsetX = map(vol, 0, 1, -10, 100);
+var offsetX2 = map (vol, 0, 1, -100, 100);
 image(img, width / 2 + offsetX, height / 2, img.width, img.height);
+image(img1, width / 2 + offsetX2 + 100, height / 2, img1.width, img1.height);
 pop();
-} else {
-fill(255);
-textSize(32);
-text("Please type your hear rate value and click submit", width / 2 - 300, height / 2);
+angle += 2;
+}
 }
 }
 
 // Constructor and particles
 // Imput data actioning changes (Spectrum and heart rate)
-
 class Particle {
 constructor() {
 this.x = random(width);
@@ -163,19 +198,17 @@ this.vx = random(-2, 2);
 this.vy = random(-2, 2);
 this.size = random(2, 5);
 }
-  
 update(spectrum) {
 this.x += this.vx;
 this.y += this.vy;
 if (this.x < 0 || this.x > width) {
 this.vx *= -1;
- }
- if (this.y < 0 || this.y > height) {
- this.vy *= -1;
- }
- this.size = map(spectrum[floor(random(spectrum.length))], 0, 255, 2, 5);
- }
-  
+}
+if (this.y < 0 || this.y > height) {
+this.vy *= -1;
+}
+this.size = map(spectrum[floor(random(spectrum.length))], 0, 255, 2, 5);
+} 
 display(spectrum, heartRate) {
 fill(map(random(spectrum.length), 0, 255, 0, 255), map(random(spectrum.length), 0, 255, 0, 255), map(random(spectrum.length), 0, 255, 0, 255));
 noStroke();
